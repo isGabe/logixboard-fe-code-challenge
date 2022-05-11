f14c70ebfadd6422cc4726be557ac29c039dfacc
Gabe Luethje <gabe.luethje@ifit.com>
2022-05-11 16:58:11 -0700

    Prettier format files

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,40 @@
-import { useRef, useState, useEffect } from 'react';
-import { createTheme, ThemeProvider } from '@material-ui/core';
-import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
-import { fetchShipments, FetchShipmentsResult, LoadingResult } from "./data/fetch-shipments"
-import { Navbar } from './components/Navbar';
-import { DashboardPage } from './pages/DashboardPage';
-import { ShipmentsPage } from './pages/ShipmentsPage';
-import './App.css';
+import { useRef, useState, useEffect } from "react";
+import { createTheme, ThemeProvider } from "@material-ui/core";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
+import {
+  fetchShipments,
+  FetchShipmentsResult,
+  LoadingResult,
+} from "./data/fetch-shipments";
+import { Navbar } from "./components/Navbar";
+import { DashboardPage } from "./pages/DashboardPage";
+import { ShipmentsPage } from "./pages/ShipmentsPage";
+import "./App.css";
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#2AC3AD'
-    }
-  }
-})
+      main: "#2AC3AD",
+    },
+  },
+});
 
 const INITIAL_RESULT: LoadingResult = {
-  status: 'LOADING'
-}
+  status: "LOADING",
+};
 
 export const App = () => {
-  const [fetchShipmentsResult, setFetchShipmentsResult] = useState<FetchShipmentsResult | LoadingResult>(INITIAL_RESULT)
-    useEffect(() => {
-        fetchShipments().then(result => setFetchShipmentsResult(result))
-    }, [])
+  const [fetchShipmentsResult, setFetchShipmentsResult] = useState<
+    FetchShipmentsResult | LoadingResult
+  >(INITIAL_RESULT);
+  useEffect(() => {
+    fetchShipments().then((result) => setFetchShipmentsResult(result));
+  }, []);
   const navBarRef = useRef<HTMLDivElement>(null);
   const [navBarHeight, setNavBarHeight] = useState<number | null>(null);
 
@@ -47,10 +58,13 @@ export const App = () => {
             <DashboardPage data={fetchShipmentsResult} />
           </Route>
           <Route path="/shipments">
-            <ShipmentsPage navBarHeight={navBarHeight} data={fetchShipmentsResult} />
+            <ShipmentsPage
+              navBarHeight={navBarHeight}
+              data={fetchShipmentsResult}
+            />
           </Route>
         </Switch>
       </Router>
     </ThemeProvider>
   );
-}
+};
